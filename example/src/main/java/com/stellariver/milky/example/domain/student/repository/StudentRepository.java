2b7a62e3b9416ca1d2ea452d3f808e6887922774
houchuang.hc <houchuang.hc@alibaba-inc.com>
2022-02-21 13:45:39 +0800

    commit

--- example/src/main/java/com/stellariver/milky/example/domain/student/repository/StudentRepository.java
+++ example/src/main/java/com/stellariver/milky/example/domain/student/repository/StudentRepository.java
@@ -1,10 +1,10 @@
 package com.stellariver.milky.example.domain.student.repository;
 
 import com.stellariver.milky.domain.support.context.Context;
-import com.stellariver.milky.domain.support.repository.DomainRepositoryService;
+import com.stellariver.milky.domain.support.repository.DomainRepository;
 import com.stellariver.milky.example.domain.student.Student;
 
-public class StudentRepository implements DomainRepositoryService<Student> {
+public class StudentRepository implements DomainRepository<Student> {
 
     @Override
     public Student getByAggregateId(String aggregateId, Context context) {
