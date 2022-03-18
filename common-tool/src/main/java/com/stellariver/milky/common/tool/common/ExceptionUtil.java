889c583b78ea4a604f59c95f84af8b93b3640d90
chuanghou <houchuang@outlook.com>
2022-03-18 19:39:26 +0800

    change some details

--- common-tool/src/main/java/com/stellariver/milky/common/tool/common/ExceptionUtil.java
+++ common-tool/src/main/java/com/stellariver/milky/common/tool/common/ExceptionUtil.java
@@ -2,7 +2,7 @@ package com.stellariver.milky.common.tool.common;
 
 public class ExceptionUtil {
 
-    static void trueThrow(boolean test, String message) {
+    public static void trueThrow(boolean test, String message) {
         if (test) {
             throw new RuntimeException(message);
         }
