721d9ea0e921240f5845d28f940e81d5e2ff51b8
chuanghou <houchuang@outlook.com>
2022-03-19 02:04:13 +0800

    change name for Interceptors.java

--- student-management/application/src/main/java/com/stellariver/milky/example/infrastructure/mapper/MybatisConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.stellariver.milky.example.infrastructure.mapper;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.stellariver.milky.example")
-public class MybatisConfiguration {
-}
--- student-management/application/src/main/java/com/stellariver/milky/example/infrastructure/mapper/UserDOMapper.java
+++
@@ -1,8 +0,0 @@
-package com.stellariver.milky.example.infrastructure.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDOMapper extends BaseMapper<UserDO> {
-}
