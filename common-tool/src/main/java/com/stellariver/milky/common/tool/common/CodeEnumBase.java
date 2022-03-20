f9d9b7236aa47b3ef5d0d5dd066df8a188b20017
chuanghou <houchuang@outlook.com>
2022-03-20 23:55:08 +0800

    change

--- common-tool/src/main/java/com/stellariver/milky/common/tool/common/CodeEnumBase.java
+++ common-tool/src/main/java/com/stellariver/milky/common/tool/common/CodeEnumBase.java
@@ -9,13 +9,13 @@ public class CodeEnumBase {
 
     public static final Code UNDEFINED = Code.code("UNDEFINED");
 
-    public static final Code PARAM_IS_NULL = Code.code("PARAM_IS_NULL");
+    public static final Code PARAM_IS_NULL = Code.code("PARAM_IS_NULL").message("参数为空");
 
-    public static final Code PARAM_FORMAT_IS_WRONG = Code.code("PARAM_FORMAT_IS_WRONG");
+    public static final Code PARAM_FORMAT_IS_WRONG = Code.code("PARAM_FORMAT_IS_WRONG").message("参数格式不正确");
 
-    public static final Code CONFIG_ERROR = Code.code("CONFIG_ERROR");
+    public static final Code CONFIG_ERROR = Code.code("CONFIG_ERROR").message("配置错误");
 
-    public static final Code CONCURRENCY_VIOLATION = Code.code("CONCURRENCY_VIOLATION");
+    public static final Code CONCURRENCY_VIOLATION = Code.code("CONCURRENCY_VIOLATION").message("并发操作失败");
 
     public static Code message(Supplier<String> supplier) {
         return CodeEnumBase.UNDEFINED.message(supplier.get());
