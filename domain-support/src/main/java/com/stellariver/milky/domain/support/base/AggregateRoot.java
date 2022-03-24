37e06e001dd2c44c5cf3b11c8219b33e5f662d93
chuanghou <houchuang@outlook.com>
2022-03-24 15:48:05 +0800

    adjust my runner

--- domain-support/src/main/java/com/stellariver/milky/domain/support/base/AggregateRoot.java
+++ domain-support/src/main/java/com/stellariver/milky/domain/support/base/AggregateRoot.java
@@ -1,9 +1,15 @@
 package com.stellariver.milky.domain.support.base;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
 /**
  * 聚合根父类
  */
-
+@Data
+@SuperBuilder
+@NoArgsConstructor
 public abstract class AggregateRoot{
 
     abstract public String getAggregateId();
--- domain-support/src/main/java/com/stellariver/milky/domain/support/repository/DomainRepository.java
+++ domain-support/src/main/java/com/stellariver/milky/domain/support/repository/DomainRepository.java
@@ -2,11 +2,13 @@ package com.stellariver.milky.domain.support.repository;
 
 import com.stellariver.milky.domain.support.context.Context;
 
+import java.util.Optional;
+
 public interface DomainRepository<T> {
 
     void save(T t, Context context);
 
-    T getByAggregateId(String aggregateId);
+    Optional<T> getByAggregateId(String aggregateId);
 
     void updateByAggregateId(T t, Context context);
 
