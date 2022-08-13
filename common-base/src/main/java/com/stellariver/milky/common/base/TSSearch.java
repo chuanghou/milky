20a989ba93bb5f890b1c31afcb6ea82c292ec210
chuanghou <houchuang@outlook.com>
2022-08-13 20:14:41 +0800

    adjust message identifier with aggregationId

--- common-base/src/main/java/com/stellariver/milky/common/base/TSSearch.java
+++ common-base/src/main/java/com/stellariver/milky/common/base/TSSearch.java
@@ -9,7 +9,7 @@ public @interface TSSearch {
     String index() default "";
 
     /**
-     * 当对应字段是字符串，在多元索引中以模糊索引存在，可以使用like租界，类似关系型数据的LIKE查询
+     * 当对应字段是字符串，在多元索引中以模糊索引存在，可以使用like注解，类似关系型数据库的LIKE查询
      * @return 是否启动模糊搜索
      */
     boolean like() default false;
