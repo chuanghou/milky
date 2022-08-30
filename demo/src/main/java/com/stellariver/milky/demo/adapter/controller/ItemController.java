19973a31927b2dd41c95a1f887d3625ab0d83293
chuanghou <houchuang@outlook.com>
2022-08-31 00:21:20 +0800

    adjust base query structure

--- demo/src/main/java/com/stellariver/milky/demo/adapter/controller/ItemController.java
+++ demo/src/main/java/com/stellariver/milky/demo/adapter/controller/ItemController.java
@@ -3,12 +3,12 @@ package com.stellariver.milky.demo.adapter.controller;
 import com.stellariver.milky.common.base.Employee;
 import com.stellariver.milky.common.base.Result;
 import com.stellariver.milky.common.tool.common.BizException;
+import com.stellariver.milky.demo.application.ItemAbility;
 import com.stellariver.milky.demo.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ItemController {
 
+    ItemAbility itemAbility;
 
     @GetMapping("publish")
     public Result<Boolean> publish(String title) {
-        Item item = =.publishItem(10086L, title);
+        Item item  = itemAbility.publishItem(10086L, title);
         return Result.success();
     }
 
@@ -27,9 +28,9 @@ public class ItemController {
     public Result<Boolean> update(Long itemId, String newTitle) {
         Employee jack = new Employee("001", "jack");
         try {
-            itemService.changeTitle(itemId, newTitle, jack);
+            itemAbility.changeTitle(itemId, newTitle, jack);
         } catch (BizException bizException) {
-            return Result.with(bizException.getErrors());
+            return Result.error(bizException.getErrors());
         }
         return Result.success();
     }
--- demo/src/main/java/com/stellariver/milky/demo/application/ItemAbility.java
+++ demo/src/main/java/com/stellariver/milky/demo/application/ItemAbility.java
@@ -6,6 +6,7 @@ import com.stellariver.milky.common.tool.util.StreamMap;
 import com.stellariver.milky.demo.basic.NameTypes;
 import com.stellariver.milky.demo.domain.item.Item;
 import com.stellariver.milky.demo.domain.item.command.ItemCreateCommand;
+import com.stellariver.milky.demo.domain.item.command.ItemTitleUpdateCommand;
 import com.stellariver.milky.demo.domain.item.command.ItemUpdateCommand;
 import com.stellariver.milky.demo.domain.item.repository.ItemRepository;
 import com.stellariver.milky.domain.support.base.NameType;
@@ -24,7 +25,7 @@ import static com.stellariver.milky.demo.basic.ErrorEnum.ITEM_NOT_EXIST;
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class ItemService {
+public class ItemAbility {
 
     ItemRepository itemRepository;
 
@@ -40,9 +41,9 @@ public class ItemService {
 
 
     public void changeTitle(Long itemId, String newTitle, Employee operator) {
-        Optional<Item> itemOptional = itemRepository.getByItemId(itemId);
+        Optional<Item> itemOptional = itemRepository.queryByIdOptional(itemId);
         BizException.trueThrow(!itemOptional.isPresent(), ITEM_NOT_EXIST.message("找不到相应item，itemId:" + itemId));
-        ItemUpdateCommand command = ItemUpdateCommand.builder().itemId(itemId).newTitle(newTitle).build();
+        ItemTitleUpdateCommand command = ItemTitleUpdateCommand.builder().itemId(itemId).updateTitle(newTitle).build();
         Map<NameType<?>, Object> parameters = StreamMap.<NameType<?>, Object>init()
                 .put(NameTypes.employee, new Employee("110", "tom"))
                 .getMap();
