7f6067b95ff96646d3b43cbdeef9214fbd839327
chuanghou <houchuang@outlook.com>
2023-01-19 15:39:07 +0800

    change around advice to before advice, clear compile warning

--- validate-tool/src/main/java/com/stellariver/milky/validate/tool/ValidateAspect.java
+++ validate-tool/src/main/java/com/stellariver/milky/validate/tool/ValidateAspect.java
@@ -1,9 +1,11 @@
 package com.stellariver.milky.validate.tool;
 
 import com.stellariver.milky.common.base.ExceptionType;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
@@ -18,17 +20,16 @@ public class ValidateAspect {
     @Pointcut("@annotation(com.stellariver.milky.validate.tool.Validate)")
     private void pointCut() {}
 
-    @Around("pointCut()")
-    public Object valid(ProceedingJoinPoint pjp) throws Throwable {
-        Object[] args = pjp.getArgs();
-        MethodSignature signature = (MethodSignature) pjp.getSignature();
+    @Before("pointCut()")
+    public void valid(JoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
         Validate annotation = method.getAnnotation(Validate.class);
         Class<?>[] groups = annotation.groups();
         boolean failFast = annotation.failFast();
         ExceptionType type = annotation.type();
-        ValidateUtil.validate(pjp.getTarget(), method, args, failFast, type, groups);
-        return pjp.proceed();
+        ValidateUtil.validate(joinPoint.getTarget(), method, args, failFast, type, groups);
     }
 
 }
