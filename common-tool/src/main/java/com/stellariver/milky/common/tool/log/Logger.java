02ca3cb505938ad7918856c97568917cb4fd83e7
chuanghou <houchuang@outlook.com>
2023-01-20 09:42:42 +0800

    Milky Logger delete complicated strategies

--- common-tool/src/main/java/com/stellariver/milky/common/tool/log/Logger.java
+++ common-tool/src/main/java/com/stellariver/milky/common/tool/log/Logger.java
@@ -1,18 +1,11 @@
 package com.stellariver.milky.common.tool.log;
 
 import com.stellariver.milky.common.tool.exception.BizException;
-import com.stellariver.milky.common.tool.common.Clock;
-import com.stellariver.milky.common.tool.util.Collect;
-import com.stellariver.milky.common.tool.util.Json;
-import lombok.EqualsAndHashCode;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.slf4j.Marker;
 
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 /**
  * @author houchuang
@@ -21,16 +14,10 @@ public class Logger implements org.slf4j.Logger {
 
     private final org.slf4j.Logger log;
 
-    private final ThreadLocal<MortalMap<String, String>> threadLocalContents = ThreadLocal.withInitial(MortalMap::new);
-
-    private final ThreadLocal<MortalMap<String, String>> originalThreadLocalContents = ThreadLocal.withInitial(MortalMap::new);
+    private final ThreadLocal<Set<String>> withKeys = ThreadLocal.withInitial(HashSet::new);
 
     static public Logger getLogger(Class<?> clazz) {
         return new Logger(clazz);
-    }
-
-    public void clear() {
-        threadLocalContents.get().clear();
     }
 
     public void log(String logTag, Throwable throwable) {
@@ -55,13 +42,13 @@ public class Logger implements org.slf4j.Logger {
                 error(logTag, throwable);
             }
         } else {
-            clear();
+            afterLog();
         }
     }
 
     public void logWhenException(String logTag, Throwable throwable) {
         if (throwable == null) {
-            threadLocalContents.get().clear();
+            afterLog();
             return;
         }
         if (throwable instanceof BizException) {
@@ -130,43 +117,24 @@ public class Logger implements org.slf4j.Logger {
             log.error("log key shouldn't be null");
             return this;
         }
-        MortalMap<String, String> contents = threadLocalContents.get();
-        if (contents.containsKey(key)) {
-            log.error("log key:{} already exists, it may from duplicate keys in one log expression, or memory leak. " +
-                    "It's very dangerous, there must have a log expression which did not end with info(), error() and so on", key);
-        }
-        String valueString;
-        if (value instanceof String) {
-            valueString = (String) value;
-        } else {
-            valueString = Json.toJson(value);
-        }
-        contents.put(key, valueString);
+        withKeys.get().add(key);
+        MDC.put(key, value.toString());
         return this;
     }
 
     private void beforeLog() {
-        Map<String, String> logContents = threadLocalContents.get();
-        logContents.forEach((k, v) -> {
-            originalThreadLocalContents.get().put(k, MDC.get(k));
-            MDC.put(k, v);
-        });
         StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
         MDC.put("milky_class", stackTraceElement.getClassName());
         MDC.put("milky_method", stackTraceElement.getMethodName());
         MDC.put("milky_line", Integer.toString(stackTraceElement.getLineNumber()));
-        logContents.clear();
     }
 
     private void afterLog() {
-        MortalMap<String, String> originalContents = originalThreadLocalContents.get();
-        if (Collect.isNotEmpty(originalContents)) {
-            originalContents.forEach(MDC::put);
-            originalContents.clear();
-        }
         MDC.remove("milky_class");
         MDC.remove("milky_method");
         MDC.remove("milky_line");
+        withKeys.get().forEach(MDC::remove);
+        withKeys.get().clear();
     }
 
     @Override
@@ -726,185 +694,4 @@ public class Logger implements org.slf4j.Logger {
         }
     }
 
-    public static class MortalMap<K, V> extends HashMap<K, V> {
-
-        public static final long DEFAULT_LIVE = 100L;
-
-
-        public static final long MIN_LIVE = 10L;
-
-
-        private final PriorityQueue<LiveKey<K>> queue = new PriorityQueue<>();
-
-        @Override
-        public V put(K key, V value) {
-            return put(key, value, DEFAULT_LIVE);
-        }
-
-        public V put(K key, V value, long liveMillis) {
-            if (key == null) {
-                throw new NullPointerException("key is null");
-            }
-            if (liveMillis <= MIN_LIVE) {
-                throw new IllegalArgumentException( "live time must be positive");
-            }
-            long currentTimeMillis = Clock.currentTimeMillis();
-            V result = super.put(key, value);
-            LiveKey<K> liveKey = new LiveKey<>(key, currentTimeMillis + liveMillis);
-            queue.remove(liveKey);
-            queue.add(liveKey);
-            removeDeath(currentTimeMillis);
-            return result;
-        }
-
-        @Override
-        public V get(Object key) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.get(key);
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.containsKey(key);
-        }
-
-        @Override
-        public boolean isEmpty() {
-            removeDeath(Clock.currentTimeMillis());
-            return super.isEmpty();
-        }
-
-        @Override
-        public boolean containsValue(Object value) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.containsValue(value);
-        }
-
-        @Override
-        public boolean replace(K key, V oldValue, V newValue) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.replace(key, oldValue, newValue);
-        }
-
-        @Override
-        public Set<K> keySet() {
-            removeDeath(Clock.currentTimeMillis());
-            return super.keySet();
-        }
-
-        @Override
-        public int size() {
-            removeDeath(Clock.currentTimeMillis());
-            return super.size();
-        }
-
-        @Override
-        public Collection<V> values() {
-            removeDeath(Clock.currentTimeMillis());
-            return super.values();
-        }
-
-        @Override
-        public void replaceAll(BiFunction<? super K, ? super V, ? extends V> function) {
-            removeDeath(Clock.currentTimeMillis());
-            super.replaceAll(function);
-        }
-
-        @Override
-        public Set<Entry<K, V>> entrySet() {
-            removeDeath(Clock.currentTimeMillis());
-            return super.entrySet();
-        }
-
-        @Override
-        public V getOrDefault(Object key, V defaultValue) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.getOrDefault(key, defaultValue);
-        }
-
-        @Override
-        public V putIfAbsent(K key, V value) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.putIfAbsent(key, value);
-        }
-
-        @Override
-        public void forEach(BiConsumer<? super K, ? super V> action) {
-            removeDeath(Clock.currentTimeMillis());
-            super.forEach(action);
-        }
-
-        @Override
-        public V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.merge(key, value, remappingFunction);
-        }
-
-        @Override
-        public V replace(K key, V value) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.replace(key, value);
-        }
-
-        @Override
-        public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.computeIfAbsent(key, mappingFunction);
-        }
-
-        @Override
-        public V computeIfPresent(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.computeIfPresent(key, remappingFunction);
-        }
-
-        @Override
-        public V compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
-            removeDeath(Clock.currentTimeMillis());
-            return super.compute(key, remappingFunction);
-        }
-
-        @Override
-        public void putAll(Map<? extends K, ? extends V> m) {
-           super.putAll(m);
-           long currentTimeMillis = Clock.currentTimeMillis();
-           m.forEach((k, v) -> queue.add(new LiveKey<>(k, currentTimeMillis + DEFAULT_LIVE)));
-           removeDeath(Clock.currentTimeMillis());
-        }
-
-        private void removeDeath(long currentTimeMillis) {
-            LiveKey<K> oldestLiveKey;
-            while (true) {
-                oldestLiveKey  = queue.peek();
-                if (oldestLiveKey == null || oldestLiveKey.deathMillis > currentTimeMillis) {
-                    break;
-                }
-                LiveKey<K> removeLiveKey = queue.remove();
-                super.remove(removeLiveKey.key);
-            }
-        }
-
-    }
-
-
-    @EqualsAndHashCode
-    static private class LiveKey<K> implements Comparable<LiveKey<K>>{
-
-        private final K key;
-
-        @EqualsAndHashCode.Exclude
-        private final long deathMillis;
-
-        public LiveKey(K key, long deathMillis) {
-            this.key = key;
-            this.deathMillis = deathMillis;
-        }
-
-        @Override
-        public int compareTo(LiveKey o) {
-            return Long.compare(this.deathMillis, o.deathMillis);
-        }
-
-    }
 }
