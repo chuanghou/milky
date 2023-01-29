e1e1e7590462546e21b6ed30c331c3811593e344
chuanghou <houchuang@outlook.com>
2023-01-29 15:09:24 +0800

    add SLambdaUtil

---
+++ common-tool/src/main/java/com/stellariver/milky/common/tool/slambda/LambdaUtils.java
@@ -0,0 +1,27 @@
+package com.stellariver.milky.common.tool.slambda;
+
+
+import com.stellariver.milky.common.tool.slambda.*;
+
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public final class LambdaUtils {
+
+    public static <T> LambdaMeta extract(SFunction<T, ?> func) {
+        // 1. IDEA 调试模式下 lambda 表达式是一个代理
+        if (func instanceof Proxy) {
+            return new IdeaProxyLambdaMeta((Proxy) func);
+        }
+        // 2. 反射读取
+        try {
+            Method method = func.getClass().getDeclaredMethod("writeReplace");
+            return new ReflectLambdaMeta((SerializedLambda) Reflection.setAccessible(method).invoke(func));
+        } catch (Throwable e) {
+            // 3. 反射失败使用序列化的方式读取
+            return new ShadowLambdaMeta(com.stellariver.milky.common.tool.slambda.SerializedLambda.extract(func));
+        }
+    }
+
+}
