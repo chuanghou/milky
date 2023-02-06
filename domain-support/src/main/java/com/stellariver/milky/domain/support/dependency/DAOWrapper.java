e9fae6072e096910baa8138ebec7d6c48e3da8ae
chuanghou <houchuang@outlook.com>
2023-02-06 12:25:48 +0800

    jacoco 与 reflectasm 兼容

--- domain-support/src/main/java/com/stellariver/milky/domain/support/dependency/DAOWrapper.java
+++ domain-support/src/main/java/com/stellariver/milky/domain/support/dependency/DAOWrapper.java
@@ -1,5 +1,6 @@
 package com.stellariver.milky.domain.support.dependency;
 
+import com.esotericsoftware.reflectasm.MethodAccess;
 import com.stellariver.milky.common.tool.exception.ErrorEnumsBase;
 import com.stellariver.milky.common.tool.common.Kit;
 import com.stellariver.milky.common.tool.exception.SysException;
@@ -8,31 +9,55 @@ import com.stellariver.milky.domain.support.ErrorEnums;
 import com.stellariver.milky.domain.support.base.BaseDataObject;
 import lombok.NonNull;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.lang.reflect.Field;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @author houchuang
  */
 public interface DAOWrapper<DataObject extends BaseDataObject<?>, PrimaryId> {
 
-    @SneakyThrows(IllegalAccessException.class)
-    default void checkNullField(Object obj) {
-        for (Field field : obj.getClass().getDeclaredFields()) {
-            field.setAccessible(true);
-            Object o = field.get(obj);
-            SysException.nullThrowGet(o, () -> {
-                String message = String.format("class %s, field %s", obj.getClass().getSimpleName(), field.getName());
-                return ErrorEnumsBase.FIELD_IS_NULL.message(message);
-            });
+    class CheckNull {
+
+        private static final Map<Class<?>, List<Pair<MethodAccess, Integer>>> gettersMap = new ConcurrentHashMap<>();
+
+        public static boolean isGetter(String name) {
+            return (name.startsWith("get") && name.length() > 3) || (name.startsWith("is") && name.length() > 2);
         }
+
+        public static void checkNullField(List<Object> objects) throws SysException {
+            Class<?> clazz = objects.get(0).getClass();
+            List<Pair<MethodAccess, Integer>> getters = gettersMap.get(clazz);
+            if (getters == null) {
+                getters = Arrays.stream(clazz.getDeclaredMethods())
+                        .filter(m -> m.getParameterTypes().length == 0 && isGetter(m.getName()))
+                        .map(method -> {
+                            MethodAccess methodAccess = MethodAccess.get(clazz);
+                            int methodIndex = methodAccess.getIndex(method.getName(), method.getParameterTypes());
+                            return Pair.of(methodAccess, methodIndex);
+                        }).collect(Collectors.toList());
+                gettersMap.put(clazz, getters);
+            }
+            for (Object object : objects) {
+                getters.forEach(getter -> {
+                    MethodAccess methodAccess = getter.getLeft();
+                    Integer index = getter.getRight();
+                    Object value = methodAccess.invoke(object, index);
+                    SysException.nullThrowGet(value, () -> ErrorEnums.FIELD_IS_NULL.message(object));
+                });
+            }
+        }
+
     }
 
     @SuppressWarnings("unchecked")
     default void batchSaveWrapper(List<Object> dataObjects) {
         if (Collect.isEmpty(dataObjects)) { return; }
-        dataObjects.forEach(this::checkNullField);
+        CheckNull.checkNullField(dataObjects);
         int count = batchSave(Collect.transfer(dataObjects, doj -> (DataObject) doj));
         SysException.trueThrow(Kit.notEq(count, dataObjects.size()), ErrorEnums.PERSISTENCE_ERROR);
     }
@@ -42,7 +67,7 @@ public interface DAOWrapper<DataObject extends BaseDataObject<?>, PrimaryId> {
     @SuppressWarnings("unchecked")
     default void batchUpdateWrapper(List<Object> dataObjects) {
         if (Collect.isEmpty(dataObjects)) { return; }
-        dataObjects.forEach(this::checkNullField);
+        CheckNull.checkNullField(dataObjects);
         int count = batchUpdate(Collect.transfer(dataObjects, doj -> (DataObject) doj));
         SysException.trueThrow(Kit.notEq(count, dataObjects.size()), ErrorEnums.PERSISTENCE_ERROR);
     }
