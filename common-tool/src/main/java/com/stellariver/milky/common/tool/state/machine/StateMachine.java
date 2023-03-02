1d4a1ec9bc068a81bf6dc7e82adad8de139d099f
chuanghou <houchuang@outlook.com>
2023-03-02 12:31:22 +0800

    Add StateMachineTest

--- common-tool/src/main/java/com/stellariver/milky/common/tool/state/machine/StateMachine.java
+++ common-tool/src/main/java/com/stellariver/milky/common/tool/state/machine/StateMachine.java
@@ -3,10 +3,13 @@ package com.stellariver.milky.common.tool.state.machine;
 import com.stellariver.milky.common.tool.common.TriConsumer;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiPredicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This a very simple state machine implementation, I have ever use squirrel state machine and spring state machine,
@@ -16,9 +19,92 @@ import java.util.function.BiPredicate;
  * @param <Event> the type of event
  */
 
+@SuppressWarnings("all")
 public class StateMachine<State, Event> {
 
     private final Map<State, Map<Event, Action<State, Event>>> transitions = new HashMap<>();
+
+
+    static private Pattern patternS = Pattern.compile("\\\"\\w+\\\"\\s*->");
+    static private Pattern patternT = Pattern.compile("->\\s*\\\"\\w+\\\"");
+    static private Pattern patternL = Pattern.compile("\\[\\s*label\\s*=\\s*\\\".+\\\"\\]");
+    static private Pattern patternE = Pattern.compile("\\\"\\s*\\w+(,|\\\")");
+    static private Pattern patternC = Pattern.compile("c\\s*\\:\\s*\\w+\\s*(\\,|\\\")");
+    static private Pattern patternR = Pattern.compile("r\\s*\\:\\s*\\w+\\s*(\\,|\\\")");
+
+    public static StateMachine<String, String> buildStateMachine(String dot) {
+
+        Matcher matcherS = patternS.matcher(dot);
+        Matcher matcherT = patternT.matcher(dot);
+        Matcher matcherL = patternL.matcher(dot);
+
+        List<Transition<String, String>> transitions = new ArrayList<>();
+        while (true) {
+            boolean s = matcherS.find();
+            boolean t = matcherT.find();
+            boolean l = matcherL.find();
+
+            if (!s && !t && !l) {
+                break;
+            }
+            if (s && t && l) {
+                String source = extractByDelimiter(matcherS.group());
+                String target = extractByDelimiter(matcherT.group());
+                String label = matcherL.group();
+
+                String condition = null;
+                Matcher matcherC = patternC.matcher(label);
+                if (matcherC.find()) {
+                    String group = matcherC.group();
+                    condition = group.substring(group.indexOf(":") + 1, group.length() - 1).trim();
+                }
+
+                String runner = null;
+                Matcher matcherR = patternR.matcher(label);
+                if (matcherR.find()) {
+                    String group = matcherR.group();
+                    runner = group.substring(group.indexOf(":") + 1, group.length() - 1).trim();
+                }
+
+                String event;
+                Matcher matcherE = patternE.matcher(label);
+                if (matcherE.find()) {
+                    String group = matcherE.group();
+                    event = group.substring(1, group.length() - 1).trim();
+                } else {
+                    throw new RuntimeException(label);
+                }
+
+//                @SuppressWarnings("unchecked")
+//                BiPredicate<String, String> conditionBean = BeanUtil.getBeanLoader() == null || condition == null ?
+//                        null : (BiPredicate<String, String>) BeanUtil.getBean(condition);
+//                @SuppressWarnings("unchecked")
+//                TriConsumer<String, String, String> actionBean = BeanUtil.getBeanLoader() == null || runner == null ?
+//                        null : (TriConsumer<String, String, String>) BeanUtil.getBean(runner);
+
+                Transition<String, String> transition = Transition.<String, String>builder()
+                        .source(source).event(event).target(target)
+//                        .condition(conditionBean)
+//                        .action(actionBean)
+                        .build();
+
+                transitions.add(transition);
+            } else {
+                throw new RuntimeException(dot);
+            }
+        }
+        return new StateMachine<>(transitions);
+    }
+
+    static private String extractByDelimiter(String str) {
+        int leftIndex = str.indexOf("\"");
+        int rightIndex = str.lastIndexOf("\"");
+        if (rightIndex - leftIndex >= 2) {
+            return str.substring(leftIndex + 1, rightIndex).trim();
+        } else {
+            throw new RuntimeException(str + ", left: " + "\"" + ", right: " + "\"");
+        }
+    }
 
     public StateMachine(List<Transition<State, Event>> transitionList) {
 
--- demo/src/test/java/com/stellariver/milky/demo/GraphVizTest.java
+++ demo/src/test/java/com/stellariver/milky/demo/GraphVizTest.java
@@ -1,84 +1,45 @@
 package com.stellariver.milky.demo;
 
 import com.stellariver.milky.common.tool.state.machine.StateMachine;
-import com.stellariver.milky.common.tool.state.machine.Transition;
-import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.util.ResourceUtils;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+
+//http://viz-js.com/
+//# http://www.graphviz.org/content/cluster
+//
+//        digraph fsm {
+//        "a" -> "a" [label= "Event1, c:condition, r: runner1"]
+//        "a" -> "b" [label= "Event2"]
+//        "b" -> "c" [label= "Event3"]
+//        "b" -> "d" [label= "Event4"]
+//        "c" -> "a" [label= "Event5"]
+//        }
 
 public class GraphVizTest {
 
     @Test
     public void test() throws IOException {
-        ClassPathResource cpr = new ClassPathResource("a.txt");
-        byte[] bdata = FileCopyUtils.copyToByteArray(cpr.getInputStream());
-        String data = new String(bdata, StandardCharsets.UTF_8);
+        String data = "# http://www.graphviz.org/content/cluster\n" +
+                "\n" +
+                "        digraph fsm {\n" +
+                "        \"a\" -> \"a\" [label= \"Event1, c:condition, r: runner1\"]\n" +
+                "        \"a\" -> \"b\" [label= \"Event2\"]\n" +
+                "        \"b\" -> \"c\" [label= \"Event3\"]\n" +
+                "        \"b\" -> \"d\" [label= \"Event4\"]\n" +
+                "        \"c\" -> \"a\" [label= \"Event5\"]\n" +
+                "        }";
 
-        // 按指定模式在字符串查找
-        String patternSource = "\\\"\\w+\\\"\\s*->";
+        StateMachine<String, String> machine = StateMachine.buildStateMachine(data);
 
-
-        String patternTarget = "->\\s*\\\"\\w+\\\"";
-
-
-        String patternEvent = "\\[\\s*label\\s*=\\s*\\\"\\s*\\w+\\s*\\\"\\s*\\]";
-
-
-
-        Pattern patternS = Pattern.compile(patternSource);
-        Pattern patternT = Pattern.compile(patternTarget);
-        Pattern patternE = Pattern.compile(patternEvent);
-        Matcher matcherS = patternS.matcher(data);
-        Matcher matcherT = patternT.matcher(data);
-        Matcher matcherE = patternE.matcher(data);
-
-        List<Transition<String, String>> transitions = new ArrayList<>();
-        while (true) {
-            boolean s = matcherS.find();
-            boolean t = matcherT.find();
-            boolean e = matcherE.find();
-
-            if (!s && !t && !e) {
-                break;
-            }
-            if (s && t && e) {
-                String sourceTemp = matcherS.group();
-                String source = extract(sourceTemp);
-                String targetTemp = matcherT.group();
-                String target = extract(targetTemp);
-                String eventTemp = matcherE.group();
-                String event = extract(eventTemp);
-                Transition<String, String> transition = Transition.<String, String>builder()
-                        .source(source).event(event).target(target).build();
-                transitions.add(transition);
-            }
-        }
-
-        StateMachine<String, String> machine = new StateMachine<>(transitions);
         String fire = machine.fire("a", "Event2");
-        System.out.println(fire);
+        Assertions.assertEquals(fire, "b");
 
+        fire = machine.fire("b", "Event2");
+        Assertions.assertNull(fire);
     }
 
-    static public Pattern quotes = Pattern.compile("\\\"\\s*\\w+\\s*\\\"");
-    private String extract(String sourceTemp) {
-        Matcher matcher = quotes.matcher(sourceTemp);
-        if (matcher.find()) {
-            String group = matcher.group();
-            return group.substring(1, group.length() - 1).trim();
-        } else {
-            throw new RuntimeException(sourceTemp);
-        }
-    }
 }
