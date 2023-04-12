1cbccd2da9d7efc90bec3fb9afc50385dde58f5d
chuanghou <houchuang@outlook.com>
2023-04-12 16:24:36 +0800

    shame! A big Bug!

--- demo/starter/src/test/java/com/stellariver/milky/starter/demo/starter/ClockTest.java
+++ demo/starter/src/test/java/com/stellariver/milky/starter/demo/starter/ClockTest.java
@@ -1,12 +1,13 @@
 package com.stellariver.milky.starter.demo.starter;
 
 import com.stellariver.milky.common.tool.common.Clock;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ClockTest {
 
     @Test
     public void testClock() {
-        System.out.println(Clock.today());
+        Assertions.assertNotEquals(Clock.today(), 0);
     }
 }
