8dee6b7854feb0a03b1fc7a49cde46b473a14aed
chuanghou <houchuang@outlook.com>
2023-05-16 10:58:20 +0800

    add throwUnreachable()

--- common-tool/src/main/java/com/stellariver/milky/common/tool/exception/SysEx.java
+++ common-tool/src/main/java/com/stellariver/milky/common/tool/exception/SysEx.java
@@ -83,4 +83,8 @@ public class SysEx extends BaseEx {
         return new SysEx(ErrorEnumsBase.UNREACHABLE_CODE);
     }
 
+    static public void throwUnreachable() {
+        throw new SysEx(ErrorEnumsBase.UNREACHABLE_CODE);
+    }
+
 }
