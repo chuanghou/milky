5ccfd33c0e5b1a209d82b7cadddff729c3c46809
chuanghou <houchuang@outlook.com>
2023-07-12 16:49:17 +0800

    add Json.parse TypeReference

---
+++ common-base/src/main/java/com/stellariver/milky/common/base/PageResult.java
@@ -0,0 +1,58 @@
+package com.stellariver.milky.common.base;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author houchuang
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PageResult<T> extends Result<List<T>> {
+
+    private final Paginator paginator = new Paginator();
+
+    private PageResult() {
+        super();
+    }
+
+    private PageResult(List<T> data, long total, long pageNo, long pageSize) {
+        this.success = true;
+        this.data = data;
+        paginator.setPageNo(pageNo);
+        paginator.setPageSize(pageSize);
+        paginator.setPageCount((total/pageSize + (total%pageSize == 0 ? 0 : 1)));
+        paginator.setTotal(total);
+    }
+
+    public Paginator getPaginator() {
+        return this.paginator;
+    }
+
+    public static <T> PageResult<T> success(List<T> data, long total, long pageNo, long pageSize) {
+        return new PageResult<>(data, total, pageNo, pageSize);
+    }
+
+    public static <T> PageResult<T> empty(long pageSize) {
+        return success(Collections.emptyList(), 0L, 1, pageSize);
+    }
+
+    public static <T> PageResult<T> pageError(ErrorEnum errorEnum, ExceptionType type) {
+        return pageError(Collections.singletonList(errorEnum), type);
+    }
+
+    public static <T> PageResult<T> pageError(List<ErrorEnum> errorEnums, ExceptionType exceptionType) {
+        PageResult<T> result = new PageResult<>();
+        result.success = false;
+        result.code = errorEnums.get(0).getCode();
+        result.detailMessage = errorEnums.get(0).getMessage();
+        result.errorEnums = errorEnums;
+        result.exceptionType = exceptionType;
+        result.message = exceptionType == ExceptionType.BIZ ? result.detailMessage : "系统繁忙，请稍后再试！";
+        return result;
+    }
+
+}
