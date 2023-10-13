d343cbd4efe8076cda2eefc110e71383da1249ac
chuanghou <houchuang@outlook.com>
2023-10-13 09:10:54 +0800

    change StaticWireApplicationRunner Highest_Precedence

--- spring-partner/src/main/java/com/stellariver/milky/spring/partner/wire/StaticWireApplicationRunner.java
+++ spring-partner/src/main/java/com/stellariver/milky/spring/partner/wire/StaticWireApplicationRunner.java
@@ -5,9 +5,10 @@ import lombok.AllArgsConstructor;
 import org.reflections.Reflections;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.Ordered;
 
 @AllArgsConstructor
-public class StaticWireApplicationRunner implements ApplicationRunner {
+public class StaticWireApplicationRunner implements ApplicationRunner, Ordered {
 
     private Reflections reflections;
 
@@ -18,6 +19,11 @@ public class StaticWireApplicationRunner implements ApplicationRunner {
 
     public void close() {
         StaticWireSupport.unWire(reflections);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 
 }
