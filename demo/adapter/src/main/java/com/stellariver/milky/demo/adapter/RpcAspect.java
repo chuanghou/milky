b771aa7435464c444aa17055785d19e129791080
houchuang <houchuang@outlook.com>
2024-02-06 17:04:19 +0800

    使用druid构建深度翻页block插件, 优化插件执行位置

--- demo/adapter/src/main/java/com/stellariver/milky/demo/adapter/RpcAspect.java
+++ demo/adapter/src/main/java/com/stellariver/milky/demo/adapter/RpcAspect.java
@@ -1,23 +1,30 @@
 package com.stellariver.milky.demo.adapter;
 
+import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.stellariver.milky.aspectj.tool.validate.AnnotationValidateAspect;
 import com.stellariver.milky.aspectj.tool.validate.Validate;
 import com.stellariver.milky.common.base.*;
+import com.stellariver.milky.common.tool.common.Kit;
 import com.stellariver.milky.common.tool.log.Logger;
 import com.stellariver.milky.common.tool.stable.MilkyStableSupport;
 import com.stellariver.milky.common.tool.stable.RateLimiterWrapper;
 import com.stellariver.milky.common.tool.util.Collect;
 import com.stellariver.milky.common.tool.validate.ValidateUtil;
 import com.stellariver.milky.domain.support.ErrorEnums;
+import lombok.NonNull;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.IntStream;
 
 /**
@@ -68,38 +75,121 @@ public class RpcAspect {
         Class<?> returnType = method.getReturnType();
         long start = System.nanoTime();
         List<ErrorEnum> errorEnums = Collections.emptyList();
-        Throwable t = null;
+        Throwable excavated = null, original = null;
+
         try {
             if (method.getAnnotation(Validate.class) == null) {
                 ValidateUtil.validate(pjp.getTarget(), method, args, true, ExceptionType.BIZ);
             }
             result = pjp.proceed();
         } catch (Throwable throwable) {
-            if (throwable instanceof BaseEx) {
+            original = throwable;
+            excavated = excavate(throwable);
+            if (excavated instanceof BaseEx) {
                 errorEnums = ((BaseEx) throwable).getErrors();
             } else {
                 ErrorEnum errorEnum = ErrorEnums.SYS_EX.message(throwable.getMessage());
                 errorEnums = Collect.asList(errorEnum);
             }
-            t = throwable;
         } finally {
-            if (t != null) {
-                ExceptionType exceptionType = t instanceof BizEx ? ExceptionType.BIZ : ExceptionType.SYS;
+
+            if (excavated != null) {
+                ExceptionType exceptionType = excavated instanceof BizEx ? ExceptionType.BIZ : ExceptionType.SYS;
                 if (returnType == Result.class) {
                     result = Result.error(errorEnums, exceptionType);
-                } else {
+                } else if (returnType == IterableResult.class){
                     result = IterableResult.pageError(errorEnums, exceptionType);
+                } else if (returnType == PageResult.class){
+                    result = PageResult.pageError(errorEnums, exceptionType);
+                } else {
+                    //noinspection ThrowFromFinallyBlock
+                    throw new SysEx(ErrorEnumsBase.UNREACHABLE_CODE);
                 }
                 if (exceptionType == ExceptionType.BIZ) {
-                    ((Result<?>) result).setMessage(t.getMessage());
-//                    ((Result<?>) result).setMessage(ExceptionUtils.getMessage(t));
+                    ((Result<?>) result).setMessage(excavated.getMessage());
                 }
             }
+
             IntStream.range(0, args.length).forEach(i -> log.with("arg" + i, args[i]));
-            String logTag = ((MethodSignature) pjp.getSignature()).getMethod().getName();
-            log.result(result).source("rpc's source").cost(System.nanoTime() - start).log(logTag, t);
+
+            String className = method.getDeclaringClass().getSimpleName();
+            String methodName = method.getName();
+            String message = Kit.op(excavated).map(Throwable::getMessage).orElse("");
+            log.result(result).source("custom").cost(System.nanoTime() - start)
+                    .position(String.format("%s_%s", className, methodName)).log(message, excavated);
+
+            if (original != null && original != excavated) {
+                log.position("excavated").error(original.getMessage(), original);
+            }
+
         }
         return result;
     }
+
+    private Throwable excavate(@NonNull Throwable original) {
+        Throwable current = original;
+        while (true) {
+            Excavator<? extends Throwable, ? extends Throwable> excavator = excavators.get(current.getClass());
+            if (excavator == null) {
+                return current;
+            }
+            Throwable throwable = excavator.excavateWrapper(current);
+            if (throwable == null) {
+                return current;
+            } else {
+                current = throwable;
+            }
+        }
+    }
+
+
+    interface Excavator<Original extends Throwable, Cause extends Throwable> {
+
+        Cause excavate(Original original);
+
+        @SuppressWarnings("unchecked")
+        default Cause excavateWrapper(Throwable throwable) {
+            return excavate((Original) throwable);
+        }
+
+    }
+
+    static class InvocationTargetExceptionExcavator implements Excavator<InvocationTargetException, Throwable> {
+
+        @Override
+        public Throwable excavate(InvocationTargetException e) {
+            return e.getTargetException();
+        }
+
+    }
+
+
+    static class UncheckedExecutionExceptionExcavator implements Excavator<UncheckedExecutionException, Throwable> {
+
+        @Override
+        public Throwable excavate(UncheckedExecutionException e) {
+            return e.getCause();
+        }
+
+    }
+
+    static class ExecutionExceptionExcavator implements Excavator<ExecutionException, Throwable> {
+
+        @Override
+        public Throwable excavate(ExecutionException e) {
+            return e.getCause();
+        }
+
+    }
+
+    static private final Map<Class<? extends Throwable>, Excavator<? extends Throwable, ? extends Throwable>> excavators = new HashMap<>();
+
+
+    static {
+        excavators.put(UncheckedExecutionException.class, new UncheckedExecutionExceptionExcavator());
+        excavators.put(ExecutionException.class, new ExecutionExceptionExcavator());
+        excavators.put(InvocationTargetException.class, new InvocationTargetExceptionExcavator());
+    }
+
 
 }
