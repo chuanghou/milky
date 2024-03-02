fa0f570e16d34733b0c113ce2a4e614e3a472d36
houchuang <houchuang@outlook.com>
2024-03-02 17:07:21 +0800

    milky log test

---
+++ demo/starter/src/test/java/com/stellariver/milky/demo/MilkLogTest.java
@@ -0,0 +1,35 @@
+package com.stellariver.milky.demo;
+
+
+import com.stellariver.milky.common.base.BeanUtil;
+import com.stellariver.milky.common.base.Employee;
+import com.stellariver.milky.common.tool.common.Typed;
+import com.stellariver.milky.common.tool.test.ParameterMatcher;
+import com.stellariver.milky.demo.basic.TypedEnums;
+import com.stellariver.milky.demo.common.enums.ChannelEnum;
+
+import com.stellariver.milky.demo.infrastructure.database.entity.ItemDO;
+import com.stellariver.milky.demo.infrastructure.database.mapper.ItemDOMapper;
+import lombok.CustomLog;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@CustomLog
+@SpringBootTest
+public class MilkLogTest {
+
+    @Autowired
+    ItemDOMapper itemDOMapper;
+
+    @Test
+    public void testMilkyLog() {
+        ItemDO itemDO = ItemDO.builder().userId(9000L)
+                .itemId(100000L).title("myTitle")
+                .amount(1000L).channelEnum(ChannelEnum.JD).storeCode("jd").price("78").userName("78").build();
+        itemDOMapper.insert(itemDO);
+        itemDOMapper.selectList(null);
+    }
+
+}
