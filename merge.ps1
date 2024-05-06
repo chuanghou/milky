6436feac987d9a91520d5680ca5d9272deef65ff
houchuang <houchuang@outlook.com>
2024-05-06 20:31:04 +0800

    it is merge test

--- merge.ps1
+++ merge.ps1
@@ -1,13 +1,19 @@
 
+#chcp UTF-8, and save original page code
 $CurrentCodePage = chcp
 $OriginalCodePage = ($CurrentCodePage -split ' ')[($CurrentCodePage -split ' ').Length - 1]
-#chcp UTF-8
 chcp 65001
+
+# join comments
+$comment = $args -join " "
+
 git add -A
-git commit -m $args[0]
+git commit -m $comment
 git push
 git checkout main
 git merge target
 git push
 git checkout target
+
+# recover original code page
 chcp $OriginalCodePage
