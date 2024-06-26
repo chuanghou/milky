50356579080a028857b41340c0b7c0deada8040c
houchuang <houchuang@outlook.com>
2024-06-26 14:02:55 +0800

    BigDecimal Value need to be stripTrailingZeros

--- common-tool/src/main/java/com/stellariver/milky/common/tool/Doubles.java
+++ common-tool/src/main/java/com/stellariver/milky/common/tool/Doubles.java
@@ -12,7 +12,7 @@ public class Doubles {
         for (double r : right) {
             l = l.add(BigDecimal.valueOf(r));
         }
-        return l.doubleValue();
+        return l.stripTrailingZeros().doubleValue();
     }
 
     public static double subtract(double left, double... right) {
@@ -20,7 +20,7 @@ public class Doubles {
         for (double r : right) {
             l = l.subtract(BigDecimal.valueOf(r));
         }
-        return l.doubleValue();
+        return l.stripTrailingZeros().doubleValue();
     }
 
     public static double multiply(double left, double... right) {
@@ -28,36 +28,36 @@ public class Doubles {
         for (double r : right) {
             l = l.multiply(BigDecimal.valueOf(r));
         }
-        return l.doubleValue();
+        return l.stripTrailingZeros().doubleValue();
     }
 
     public static double divide(double left, double right, int scale, RoundingMode roundingMode) {
-        return BigDecimal.valueOf(left).divide(BigDecimal.valueOf(right), scale, roundingMode).doubleValue();
+        return BigDecimal.valueOf(left).divide(BigDecimal.valueOf(right), scale, roundingMode).stripTrailingZeros().doubleValue();
     }
 
     public static double divide(double left, double right, int scale) {
-        return BigDecimal.valueOf(left).divide(BigDecimal.valueOf(right), scale, RoundingMode.HALF_UP).doubleValue();
+        return BigDecimal.valueOf(left).divide(BigDecimal.valueOf(right), scale, RoundingMode.HALF_UP).stripTrailingZeros().doubleValue();
     }
 
     public static Pair<Double, Double> divideAndRemainder(double left, double right) {
         BigDecimal[] bigDecimals = BigDecimal.valueOf(left).divideAndRemainder(BigDecimal.valueOf(right));
-        return Pair.of(bigDecimals[0].doubleValue(), bigDecimals[1].doubleValue());
+        return Pair.of(bigDecimals[0].stripTrailingZeros().doubleValue(), bigDecimals[1].stripTrailingZeros().doubleValue());
     }
 
     public static double divideToIntegralValue(double left, double right) {
-        return BigDecimal.valueOf(left).divideToIntegralValue(BigDecimal.valueOf(right)).doubleValue();
+        return BigDecimal.valueOf(left).divideToIntegralValue(BigDecimal.valueOf(right)).stripTrailingZeros().doubleValue();
     }
 
     public static double remainder(double left, double right) {
-        return BigDecimal.valueOf(left).remainder(BigDecimal.valueOf(right)).doubleValue();
+        return BigDecimal.valueOf(left).remainder(BigDecimal.valueOf(right)).stripTrailingZeros().doubleValue();
     }
 
     public static double scale(double value, int bits, RoundingMode roundingMode) {
-        return BigDecimal.valueOf(value).setScale(bits, roundingMode).doubleValue();
+        return BigDecimal.valueOf(value).setScale(bits, roundingMode).stripTrailingZeros().doubleValue();
     }
 
     public static double scale(double value, int bits) {
-        return BigDecimal.valueOf(value).setScale(bits, RoundingMode.HALF_UP).doubleValue();
+        return BigDecimal.valueOf(value).setScale(bits, RoundingMode.HALF_UP).stripTrailingZeros().doubleValue();
     }
 
 }
