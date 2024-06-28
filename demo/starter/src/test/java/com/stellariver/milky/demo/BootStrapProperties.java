9c9b49176f69a5b563ffd063ccdfa0253e827386
houchuang <houchuang@outlook.com>
2024-06-28 20:52:54 +0800

    finish BootStrapProperties

--- demo/starter/src/test/java/com/stellariver/milky/demo/BootStrapProperties.java
+++ demo/starter/src/test/java/com/stellariver/milky/demo/BootStrapProperties.java
@@ -1,7 +1,9 @@
 package com.stellariver.milky.demo;
 
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
+@ContextConfiguration
 @TestPropertySource("classpath:bootstrap.properties")
 public abstract class BootStrapProperties {
 }
