010320afb8ddfd1a395e957e662f5b460215944b
houchuang <houchuang@outlook.com>
2024-06-29 13:17:10 +0800

    Revert "finish BootStrapProperties"
    This reverts commit 9c9b49176f69a5b563ffd063ccdfa0253e827386.

--- demo/starter/src/test/java/com/stellariver/milky/demo/BootStrapProperties.java
+++ demo/starter/src/test/java/com/stellariver/milky/demo/BootStrapProperties.java
@@ -1,9 +1,7 @@
 package com.stellariver.milky.demo;
 
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
-@ContextConfiguration
 @TestPropertySource("classpath:bootstrap.properties")
 public abstract class BootStrapProperties {
 }
